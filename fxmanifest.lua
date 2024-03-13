a24cf430bfb0f1089a5f9fabfe152b6502f34340
Spirit <97816274+SpiritsCreations@users.noreply.github.com>
2024-03-13 20:27:44 +0000

    Alpha

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,4 +3,10 @@ games { 'rdr3', 'gta5' }
 
 author "Spirit's Creations"
 description 'Lore-Friendly Car Pack'
-version '0.1 Alpha'
+version '1.0 Alpha'
+
+client_scripts {
+    'vehicle_names.lua'
+}
+
+server_script 'server.lua'
